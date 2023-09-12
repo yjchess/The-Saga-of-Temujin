2df7bf35425c0b07a902842de73f4a8b89ec12eb
Yang-Jian Zhou <yangjian.zhou@live.co.uk>
2023-09-12 23:46:45 +0100

    Revert "fix: revert bad change"
    This reverts commit 78dfe6c253d43a2811522143a9f4fa019bbc6ecd.

--- models/MapModel.js
+++ models/MapModel.js
@@ -1,1 +1,47 @@
-class Map{constructor(r){1===r&&(this.aestheticFeatures=[[[1,1],[25,6],"dirt"],[[19,21],[24,24],"dirt"],[[12,11],[14,14],"bridge"],],this.impassableFeatures=[[[1,12],[11,13],"river"],[[15,12],[25,13],"river"],],this.buildableFeatures=[[[2,2],[24,5],"dark_dirt"],[[20,22],[23,23],"dark_dirt"],])}checkPassableTerrain([r,_]){return this.impassableFeatures.forEach(e=>{if(r>=e[0][0]&&r<=e[1][0]&&_>=e[0][1]&&_<=e[1][1])return!1}),!0}}
+class Map{
+    constructor(level){
+
+        switch (level) {
+            case 1:
+                this.aestheticFeatures = [
+                    //(inclusive) Bottom Left, Top Right, Type
+                    [[1,1], [25,6],"dirt"],
+                    [[19,21], [24,24],"dirt"],
+                    [[12,11], [14,14],"bridge"],
+                ]
+
+                this.impassableFeatures = [
+                    [[1,12], [11,13],"river"],
+                    [[15,12], [25,13],"river"],
+                ]
+
+                this.buildableFeatures = [
+                    [[2,2], [24,5],"dark_dirt"],
+                    [[20,22], [23,23],"dark_dirt"],
+                ]
+
+                break;
+        
+            default:
+                break;
+        }
+
+    }
+
+    checkPassableTerrain([x,y]){
+        this.impassableFeatures.forEach((feature)=>{
+            if(x >= feature[0][0] && x <= feature[1][0]){
+                if (y >= feature[0][1] && y <= feature[1][1]){
+                    return false
+                }
+            }
+        });
+
+        return true;
+    }
+
+
+        
+
+
+}
--- models/UnitModel.js
+++ models/UnitModel.js
@@ -1,1 +1,234 @@
-const VILLAGER={name:"villager",health:10,range:2,damage:1,movement:1},CAVALRY={name:"cavalry",health:15,range:2,damage:5,movement:3};class Unit{constructor(e,t,s,i){this.owner=e,this.name=t.name,this.health=t.health,this.range=t.range,this.damage=t.damage,this.movement=t.movement,this.coord=s,this.movable_events=[],this.attackable_events=[],this.selectable_events=[],this.moved=!1,this.attacked=!1,this.selected=!1,this.destroyed=!1,this.AI_behaviour=i}calculatePossibleMoves(){if(!1===this.moved){let e=movementRangeSquares(this.coord,this.movement),t=[];return e.forEach(e=>{controller.checkMovable(e)&&e.toString()!=this.coord.toString()&&t.push(e)}),t}}calculatePossibleAttacks(){let e=[];if(!1===this.attacked){let t=calculateBounds(this.coord[0],this.range),s=calculateBounds(this.coord[1],this.range);for(let i=t[0];i<=t[1];i++)for(let o=s[0];o<=s[1];o++){let r=[i,o],a;a="player"===this.owner?"computer":"player",void 0!==getEntityAt(r)&&getEntityAt(r).owner===a&&e.push(r)}}return e}calculatePossibleBuilds(e){alert("Future Feature")}move(e){this.coord=e,this.moved=!0,controller.updateTakenSpots()}resetMovement(){this.moved=!1,this.attacked=!1}attack(e){let t=getEntityAt(e);t.health-=this.damage,t.health<=0&&(t.destroySelf(),controller.updateTakenSpots()),this.attacked=!0,removeAllOverlays(),controller.refreshEvents()}toggleSelection(){this.selected=!this.selected}findClosestPath(e){let t=this.calculatePossibleMoves();if(t.includes(e))controller.view.eraseUnit(this,this.owner),this.move(e),controller.view.drawUnit(this,this.owner);else{let s=26,i=26,o,r;t.forEach(t=>{Math.abs(e[1]-t[1])<i&&(i=Math.abs(e[1]-t[1]),r=t[1])}),t.forEach(t=>{t[1]===r&&Math.abs(e[0]-t[0])<s&&(s=Math.abs(e[0]-t[0]),o=t[0])}),controller.view.eraseUnit(this,this.owner),this.move([o,r]),controller.view.drawUnit(this,this.owner)}}build(e,t,s){let i=new Building(this.owner,e,t);controller.view.displayBuilding(i),"player"===this.owner&&controller.player.buildings.push(new Building("player",e,t)),"computer"===this.owner&&controller.computer.buildings.push(new Building("computer",e,t,s))}findClosestEnemy(){let e=26,t=controller.player.units[0];return controller.player.units.forEach(s=>{let i=s.coord[1]-this.coord[1];i<e&&(e=i,t=s)}),t.coord}destroySelf(){if("player"===this.owner){for(let[e,t]of controller.player.units.entries())if(t===this&&!1===this.destroyed){controller.player.units.splice(e,1),controller.view.eraseUnit(this,this.owner),this.destroyed=!0;return}}if("computer"===this.owner){for(let[s,i]of controller.computer.units.entries())if(i===this&&!1===this.destroyed){controller.computer.units.splice(s,1),controller.view.eraseUnit(this,this.owner),this.destroyed=!0,0===controller.computer.units.length&&alert("You Win!");return}}}}
+
+const VILLAGER = {name:"villager", health:10, range:2, damage:1, movement:1}
+const CAVALRY = {name:"cavalry", health:15, range:2, damage:5, movement:3}
+
+
+class Unit{
+    constructor(owner, base_stats, coord, AI_behaviour){
+        this.owner = owner,
+
+        this.name = base_stats.name,
+        this.health = base_stats.health,
+        this.range = base_stats.range,
+        this.damage = base_stats.damage,
+        this.movement = base_stats.movement,
+        this.coord = coord;
+
+        this.movable_events = [];
+        this.attackable_events = [];
+        this.selectable_events = [];
+        
+        this.moved = false;
+        this.attacked = false;
+        this.selected = false;
+
+        this.destroyed = false;
+
+
+        this.AI_behaviour = AI_behaviour;
+    }
+
+    calculatePossibleMoves(){
+        if(this.moved === false){
+            let potentialSpots = movementRangeSquares(this.coord, this.movement);
+            let possibleSpots = []
+            potentialSpots.forEach((spot)=>{
+
+                if (controller.checkMovable(spot) && !(spot.toString() == this.coord.toString())){
+                    possibleSpots.push(spot);
+                }
+            })
+            
+            return (possibleSpots);
+        }
+
+    }
+
+    calculatePossibleAttacks(){
+
+        let attackableSquares = [];
+        if (this.attacked === false){
+            let x_range = calculateBounds(this.coord[0], this.range);
+            let y_range = calculateBounds(this.coord[1], this.range);
+
+            for (let x = x_range[0]; x <= x_range[1]; x++) {
+                for (let y = y_range[0]; y <= y_range[1]; y++) {
+                    // let potentialSpot = document.querySelector(`.x${x}.y${y}`);
+                    let potentialSpot = [x,y];
+                    let enemy;
+
+                    if (this.owner === "player"){
+                        enemy = "computer"
+                    }
+                    else{
+                        enemy = "player"
+                    }
+
+
+                    // if(potentialSpot.classList.contains(enemy) || potentialSpot.childNodes[0].classList.contains(enemy)){
+                    //     attackableSquares.push(potentialSpot);
+                    // }
+                    if((getEntityAt(potentialSpot)!== undefined && getEntityAt(potentialSpot).owner === enemy)){
+                        attackableSquares.push(potentialSpot);
+                    }
+                }                
+            }
+        }
+
+        return (attackableSquares);
+
+    }
+
+    calculatePossibleBuilds(buildingName){
+        alert("Future Feature");
+    }
+
+    move(coord){
+        this.coord = coord;
+        this.moved = true;
+        
+        controller.updateTakenSpots();
+    }
+
+    resetMovement(){
+        this.moved = false;
+        this.attacked = false;
+    }
+
+    attack(location){
+        let enemy = getEntityAt(location);
+        enemy.health -= this.damage;
+
+        if(enemy.health <= 0){
+            enemy.destroySelf();
+            controller.updateTakenSpots();
+        }
+        
+        this.attacked = true;
+        removeAllOverlays();
+        controller.refreshEvents();
+    }
+
+
+    toggleSelection(){
+        this.selected = !(this.selected);
+    }
+
+    findClosestPath(destination){
+        let possibleMoves = this.calculatePossibleMoves();
+        // possibleMoves.push(this.coord);
+        // console.log(possibleMoves, this);
+        if (possibleMoves.includes(destination)){
+            controller.view.eraseUnit(this, this.owner);
+            this.move(destination);
+            controller.view.drawUnit(this, this.owner);
+        }
+
+        else{
+            
+            let distanceX = 26;
+            let distanceY = 26;
+
+            let moveX;
+            let moveY;
+
+            possibleMoves.forEach((move)=>{
+                // console.log(move, destination, Math.abs(destination[1]-move[1]));
+                if (Math.abs(destination[1] - move[1]) < distanceY){
+                    distanceY = Math.abs(destination[1] - move[1]);
+                    moveY = move[1];
+                }
+            })
+
+            possibleMoves.forEach((move)=>{
+                if(move[1] === moveY){
+                    if (Math.abs(destination[0] - move[0]) < distanceX){
+                        distanceX = Math.abs(destination[0] - move[0])
+                        moveX = move[0];
+                    }
+                }
+
+            })
+
+
+            controller.view.eraseUnit(this, this.owner);
+            this.move([moveX,moveY]);
+            controller.view.drawUnit(this, this.owner);
+        }
+
+    }
+
+    build(building, coord, unitPref){
+
+        let buildingEntity = new Building(this.owner, building, coord)
+        controller.view.displayBuilding(buildingEntity);
+
+        if(this.owner === "player"){
+            controller.player.buildings.push(new Building ("player", building, coord))
+        }
+
+        if(this.owner === "computer"){
+            controller.computer.buildings.push(new Building ("computer", building, coord, unitPref))
+        }
+
+    }
+
+    findClosestEnemy(){
+        let closestDistance = 26;
+        // console.log(controller.player.units);
+        let selectedUnit = controller.player.units[0];
+
+        controller.player.units.forEach((unit)=>{
+            // let x_gap = unit.coord[0] - this.coord[0];
+            let y_gap = unit.coord[1] - this.coord[1];
+
+            // if (x_gap < closestDistance){
+            //     closestDistance = x_gap;
+            //     selectedUnit = unit;
+            // }
+            if (y_gap < closestDistance){
+                closestDistance = y_gap;
+                selectedUnit = unit;
+            }
+        });
+
+        return selectedUnit.coord;
+    }
+
+    destroySelf(){
+        if(this.owner === "player"){
+            for (const [index, unit] of controller.player.units.entries()) {
+                if ((unit === this) && (this.destroyed === false)){
+
+                    controller.player.units.splice(index, 1);
+
+                    controller.view.eraseUnit(this, this.owner);
+                    this.destroyed = true;
+                    return;
+                }
+                
+            }
+        }
+
+        if(this.owner === "computer"){
+            for (const [index, unit] of controller.computer.units.entries()) {
+                if ((unit === this) && (this.destroyed === false)){
+
+                    controller.computer.units.splice(index, 1);
+
+                    controller.view.eraseUnit(this, this.owner);
+                    this.destroyed = true;
+
+                    if(controller.computer.units.length === 0){
+                        alert("You Win!")
+                    }
+                    return;
+                }
+                
+            }
+        }
+
+
+    }
+
+}
